916063fb5ed39fa9b60b2b5027f7cf77130fc323
DuflotMaxime <32262840+DuflotMaxime@users.noreply.github.com>
2018-03-26 12:04:28 +0200

    Création PDF
    Création PDF des interventions

---
+++ pdf.php
@@ -0,0 +1,49 @@
+<?php
+ session_start();
+  try
+  {
+  	$bdd = new PDO('mysql:host=localhost;dbname=ppe;charset=utf8', 'root', '');
+  }
+  catch (Exception $e)
+  {
+  	die('Erreur : ' . $e->getMessage());
+  }
+  
+  require('fpdf.php');
+
+$pdf = new FPDF("P","cm","A4");
+
+//Titres des colonnes
+$header=array("Intervention", "Date visite","Heure visite","Matricule","Numero client");
+
+$pdf->SetFont('Arial','B',14);
+$pdf->AddPage();
+$pdf->SetFillColor(96,96,96);
+$pdf->SetTextColor(255,255,255);
+
+$reponse = $bdd->query('SELECT * FROM intervention ');
+
+$pdf->SetXY(3,3);
+for($i=0;$i<sizeof($header);$i++)
+    $pdf->cell(3.5,1,$header[$i],1,0,'C',1);
+  
+$pdf->SetFillColor(0xdd,0xdd,0xdd);
+$pdf->SetTextColor(0,0,0);
+$pdf->SetFont('Arial','',10);
+$pdf->SetXY(3,$pdf->GetY()+1);
+$fond=0;
+
+while($donnees = $reponse->fetch())
+  {
+   $pdf->cell(3.5,0.7,$donnees['NumIntervention'],1,0,'C',$fond);
+   $pdf->cell(3.5,0.7,$donnees['DateVisite'],1,0,'C',$fond);
+   $pdf->cell(3.5,0.7,$donnees['HeureVisite'],1,0,'C',$fond);
+   $pdf->cell(3.5,0.7,$donnees['Matricule'],1,0,'C',$fond);
+   $pdf->cell(3.5,0.7,$donnees['NumClient'],1,0,'C',$fond);
+   $pdf->SetXY(3,$pdf->GetY()+0.7);
+   $fond=!$fond;
+  }
+
+
+$pdf->Output();
+?>
